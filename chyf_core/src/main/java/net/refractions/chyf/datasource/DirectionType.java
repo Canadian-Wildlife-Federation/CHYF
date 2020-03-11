6b4377df6a88107a78d497a3fd29d6fcebff13c6
egouge <egouge@refractions.net>
2020-03-11 15:22:19 -0700

    refactoring adding core library

---
+++ chyf_core/src/main/java/net/refractions/chyf/datasource/DirectionType.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Government of Canada
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package net.refractions.chyf.datasource;
+
+/**
+ * Values for direction attribute on eflowpaths
+ * 
+ * @author Emily
+ *
+ */
+public enum DirectionType {
+
+	UNKNOWN(-1),
+	KNOWN(1);
+	
+	private int type;
+	
+	DirectionType(int type) {
+		this.type = type;
+	}
+	
+	public int getChyfValue() {
+		return this.type;
+	}
+	
+	public static DirectionType parseValue(Integer type) throws Exception {
+		if (type == null) return DirectionType.UNKNOWN;
+		for (DirectionType t : DirectionType.values()) {
+			if (t.type == type) return t;
+		}
+		throw new Exception(type + " is an invalid value for direction attribute");
+	}
+}
---
+++ chyf_core/src/main/java/net/refractions/chyf/datasource/RankType.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Government of Canada
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package net.refractions.chyf.datasource;
+
+/**
+ * Represents the rank value associated with an eflowpath
+ * 
+ * @author Emily
+ *
+ */
+public enum RankType{
+	PRIMARY(1),
+	SECONDARY(2);
+	
+	private int type;
+	
+	RankType(int type){
+		this.type = type;
+	}
+	
+	public int getChyfValue() {
+		return this.type;
+	}
+	
+	public static RankType parseValue(int type) {
+		for (RankType t : RankType.values()) {
+			if (t.getChyfValue() == type) return t;
+		}
+		throw new RuntimeException("The value of " + type + " is not supported for the Rank attribute.");
+	}
+}
