5135510909a8e5b3557f6d0eb682b1cfb572d30c
Emily Gouge <egouge@refractions.net>
2021-03-02 15:31:33 -0800

    update license and copyright details

--- chyf-web/src/main/java/net/refractions/chyf/rest/controllers/VectorTileController.java
+++ chyf-web/src/main/java/net/refractions/chyf/rest/controllers/VectorTileController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Canadian Wildlife Federation
+ * Copyright 2021 Canadian Wildlife Federation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
