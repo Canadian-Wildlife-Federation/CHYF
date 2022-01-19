7780066216819b5b18a30a5c38d80efb9d4bf4e3
Emily Gouge <egouge@refractions.net>
2022-01-19 14:16:19 -0800

    cors

--- chyf-web/src/main/java/net/refractions/chyf/controller/VectorTileController.java
+++ chyf-web/src/main/java/net/refractions/chyf/controller/VectorTileController.java
@@ -27,7 +27,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,7 +38,6 @@ import net.refractions.chyf.model.VectorTileLayer;
 
 @RestController
 @RequestMapping("/tiles")
-@CrossOrigin
 public class VectorTileController {
 
 	static final Logger logger = LoggerFactory.getLogger(VectorTileController.class.getCanonicalName());
