f2c8a7b4b2fdc097c4b3237cd7c7c891ad880e3a
Emily Gouge <egouge@refractions.net>
2022-11-03 09:51:28 -0700

    fix path issues

--- chyf-processing-tools/cycle-check.sh
+++ chyf-processing-tools/cycle-check.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp lib/*;lib-chyf/chyf-core-${chyf.core.version}.jar;lib-chyf/chyf-flowpath-constructor-${flowpath.constructor.version}.jar net.refractions.chyf.flowpathconstructor.directionalize.CycleCheckEngine $@
+java -cp lib/*:lib-chyf/chyf-core-${chyf.core.version}.jar:lib-chyf/chyf-flowpath-constructor-${flowpath.constructor.version}.jar net.refractions.chyf.flowpathconstructor.directionalize.CycleCheckEngine $@
